c25cba0fdbc98bd24eaa7eb405817d3a4325e5a2
Robert Donnelly <dev@robdonn.com>
2021-09-04 01:36:00 +0100

    feat: update tests and examples

--- src/__tests__/checkPublicPath.test.ts
+++ src/__tests__/checkPublicPath.test.ts
@@ -2,6 +2,9 @@ import { WebpackConfig } from '../plugin';
 import { checkPublicPath } from '../checkPublickPath';
 
 const warn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+const exit = jest
+  .spyOn(process, 'exit')
+  .mockImplementation(() => undefined as never);
 
 describe('checkPublicPath', () => {
   afterEach(() => {
@@ -13,7 +16,7 @@ describe('checkPublicPath', () => {
     ['publicPath is undefined', { output: {} }],
     ['publicPath is empty string', { output: { publicPath: '' } }],
   ])('should make publicPath undefined when %s', (_, config) => {
-    checkPublicPath(config);
+    checkPublicPath(config, {});
 
     expect(config?.output?.publicPath).toBeUndefined();
   });
@@ -23,12 +26,30 @@ describe('checkPublicPath', () => {
     ['root path', '/path'],
   ])('should warn when publicPath is %s', (_, publicPath) => {
     const config: WebpackConfig = { entry: [], output: { publicPath } };
-    checkPublicPath(config);
+    checkPublicPath(config, {});
     expect(warn).toHaveBeenCalledTimes(1);
     expect(warn).toHaveBeenNthCalledWith(
       1,
       'Using a relative or root publicPath may cause issues with loading federated modules.\nIt is advised to use the full host name if known, or set to "undefined" and allow Webpack to determine it for you.'
     );
+
+    expect(exit).toHaveBeenCalledTimes(1);
+    expect(exit).toHaveBeenNthCalledWith(1, 1);
+  });
+
+  it('should warn but NOT exit if "ignorePublicPath" option is true', () => {
+    const config: WebpackConfig = {
+      entry: [],
+      output: { publicPath: './path' },
+    };
+    checkPublicPath(config, { ignorePublicPath: true });
+    expect(warn).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenNthCalledWith(
+      1,
+      'Using a relative or root publicPath may cause issues with loading federated modules.\nIt is advised to use the full host name if known, or set to "undefined" and allow Webpack to determine it for you.'
+    );
+
+    expect(exit).toHaveBeenCalledTimes(0);
   });
 
   it('should NOT throw warning when publicPath is url without protocol', () => {
@@ -36,7 +57,7 @@ describe('checkPublicPath', () => {
       entry: [],
       output: { publicPath: '//localhost:1234' },
     };
-    checkPublicPath(config);
+    checkPublicPath(config, {});
     expect(warn).toHaveBeenCalledTimes(0);
   });
 });
